14f4ad3c63cf94bfba5f657ac653947de16b469d
WangMda <101039390+WangMda007@users.noreply.github.com>
2025-04-13 14:17:53 +0800

    Add files via upload

---
+++ aop/BeforeAdvice.java
@@ -0,0 +1,17 @@
+package com.mini.springframework.aop;
+
+import org.aopalliance.aop.Advice;
+
+/**
+ * Common marker interface for before advice, such as {@link MethodBeforeAdvice}.
+ *
+ *
+ *
+ *
+ *
+ *
+ *   
+ */
+public interface BeforeAdvice extends Advice {
+
+}
---
+++ aop/framework/autoproxy/DefaultAdvisorAutoProxyCreator.java
@@ -0,0 +1,109 @@
+package com.mini.springframework.aop.framework.autoproxy;
+
+import com.mini.springframework.aop.*;
+import com.mini.springframework.aop.aspectj.AspectJExpressionPointcutAdvisor;
+import com.mini.springframework.aop.framework.ProxyFactory;
+import com.mini.springframework.beans.BeansException;
+import com.mini.springframework.beans.PropertyValues;
+import com.mini.springframework.beans.factory.BeanFactory;
+import com.mini.springframework.beans.factory.BeanFactoryAware;
+import com.mini.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import com.mini.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.MethodInterceptor;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * BeanPostProcessor implementation that creates AOP proxies based on all candidate
+ * Advisors in the current BeanFactory. This class is completely generic; it contains
+ * no special code to handle any particular aspects, such as pooling aspects.
+ * <p>
+ *
+ *
+ *
+ *
+ *
+ *   
+ */
+public class DefaultAdvisorAutoProxyCreator implements InstantiationAwareBeanPostProcessor, BeanFactoryAware {
+
+    private DefaultListableBeanFactory beanFactory;
+
+    //暴露代理类引用
+    private final Set<Object> earlyProxyReferences = Collections.synchronizedSet(new HashSet<Object>());
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = (DefaultListableBeanFactory) beanFactory;
+    }
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+        return true;
+    }
+
+    private boolean isInfrastructureClass(Class<?> beanClass) {
+        return Advice.class.isAssignableFrom(beanClass) || Pointcut.class.isAssignableFrom(beanClass) || Advisor.class.isAssignableFrom(beanClass);
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (!earlyProxyReferences.contains(beanName)) {
+            return wrapIfNecessary(bean, beanName);
+        }
+
+        return bean;
+    }
+
+    protected Object wrapIfNecessary(Object bean, String beanName) {
+        if (isInfrastructureClass(bean.getClass())) return bean;
+
+        Collection<AspectJExpressionPointcutAdvisor> advisors = beanFactory.getBeansOfType(AspectJExpressionPointcutAdvisor.class).values();
+
+        for (AspectJExpressionPointcutAdvisor advisor : advisors) {
+            ClassFilter classFilter = advisor.getPointcut().getClassFilter();
+            // 过滤匹配类
+            if (!classFilter.matches(bean.getClass())) continue;
+
+            AdvisedSupport advisedSupport = new AdvisedSupport();
+
+            TargetSource targetSource = new TargetSource(bean);
+            advisedSupport.setTargetSource(targetSource);
+            advisedSupport.setMethodInterceptor((MethodInterceptor) advisor.getAdvice());
+            advisedSupport.setMethodMatcher(advisor.getPointcut().getMethodMatcher());
+            advisedSupport.setProxyTargetClass(true);
+
+            // 返回代理对象
+            return new ProxyFactory(advisedSupport).getProxy();
+        }
+
+        return bean;
+    }
+
+    //返回代理类
+    @Override
+    public Object getEarlyBeanReference(Object bean, String beanName) {
+        earlyProxyReferences.add(beanName);
+        return wrapIfNecessary(bean, beanName);
+    }
+
+    @Override
+    public PropertyValues postProcessPropertyValues(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+        return pvs;
+    }
+
+}
